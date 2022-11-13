03c928981d8ac456b192b1bbddf43ab224fc0970
redthing1 <redthing1@alt.icu>
2022-11-12 17:29:56 -0800

    working scaling i guess

--- demo/source/app.d
+++ demo/source/app.d
@@ -32,8 +32,8 @@ import raylib_nuklear;
 int main() {
     // Initialization
     //--------------------------------------------------------------------------------------
-    const int screenWidth = 700;
-    const int screenHeight = 394;
+    const int screenWidth = 700 * 2;
+    const int screenHeight = 394 * 2;
     InitWindow(screenWidth, screenHeight, "[raylib-nuklear] example");
 
     SetTargetFPS(60); // Set our game to run at 60 frames-per-second
@@ -42,6 +42,7 @@ int main() {
     /* GUI */
     auto bg = ColorToNuklearF(Colors.SKYBLUE);
     auto ctx = InitNuklear(10);
+    ctx.backend_render_scale = 2;
 
     // Main game loop
     while (!WindowShouldClose()) // Detect window close button or ESC key
