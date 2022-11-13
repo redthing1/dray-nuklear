b38bf98302ce14dfd59670e3cea316169b47ab34
redthing1 <redthing1@alt.icu>
2022-11-13 02:01:21 -0800

    working tabbed ui

--- demo/source/app.d
+++ demo/source/app.d
@@ -99,34 +99,6 @@ int main() {
                 nk_panel_flags.NK_WINDOW_BORDER | nk_panel_flags.NK_WINDOW_TITLE)) {
             enum EASY = 0;
             enum HARD = 1;
-            static int op = EASY;
-            static int property = 20;
-            nk_layout_row_static(ctx, 30, 80, 1);
-            if (nk_button_label(ctx, "button"))
-                TraceLog(TraceLogLevel.LOG_INFO, "button pressed");
-
-            nk_layout_row_dynamic(ctx, 30, 2);
-            if (nk_option_label(ctx, "easy", op == EASY))
-                op = EASY;
-            if (nk_option_label(ctx, "hard", op == HARD))
-                op = HARD;
-
-            nk_layout_row_dynamic(ctx, 25, 1);
-            nk_property_int(ctx, "Compression:", 0, &property, 100, 10, 1);
-
-            nk_layout_row_dynamic(ctx, 20, 1);
-            nk_label(ctx, "background:", nk_text_alignment.NK_TEXT_LEFT);
-            nk_layout_row_dynamic(ctx, 25, 1);
-            if (nk_combo_begin_color(ctx, nk_rgb_cf(bg), nk_vec2(nk_widget_width(ctx), 400))) {
-                nk_layout_row_dynamic(ctx, 120, 1);
-                bg = nk_color_picker(ctx, bg, nk_color_format.NK_RGBA);
-                nk_layout_row_dynamic(ctx, 25, 1);
-                bg.r = nk_propertyf(ctx, "#R:", 0, bg.r, 1.0f, 0.01f, 0.005f);
-                bg.g = nk_propertyf(ctx, "#G:", 0, bg.g, 1.0f, 0.01f, 0.005f);
-                bg.b = nk_propertyf(ctx, "#B:", 0, bg.b, 1.0f, 0.01f, 0.005f);
-                bg.a = nk_propertyf(ctx, "#A:", 0, bg.a, 1.0f, 0.01f, 0.005f);
-                nk_combo_end(ctx);
-            }
 
             nk_style_push_vec2(ctx, &ctx.style.window.spacing, nk_vec2(0, 0));
             nk_style_push_float(ctx, &ctx.style.button.rounding, 0);
@@ -140,6 +112,44 @@ int main() {
             if (nk_tab(ctx, "TAB2", tab_state == TAB2)) {
                 tab_state = TAB2;
             }
+
+            if (tab_state == TAB1) {
+                static int op = EASY;
+
+                nk_layout_row_dynamic(ctx, PAD, 1);
+                
+                nk_layout_row_static(ctx, 30, 80, 1);
+                if (nk_button_label(ctx, "button"))
+                    TraceLog(TraceLogLevel.LOG_INFO, "button pressed");
+
+                nk_layout_row_dynamic(ctx, 30, 2);
+                if (nk_option_label(ctx, "easy", op == EASY))
+                    op = EASY;
+                if (nk_option_label(ctx, "hard", op == HARD))
+                    op = HARD;
+            } else if (tab_state == TAB2) {
+                static int property = 20;
+
+                nk_layout_row_dynamic(ctx, PAD, 1);
+
+                nk_layout_row_dynamic(ctx, 25, 1);
+                nk_property_int(ctx, "Compression:", 0, &property, 100, 10, 1);
+
+                nk_layout_row_dynamic(ctx, 20, 1);
+                nk_label(ctx, "background:", nk_text_alignment.NK_TEXT_LEFT);
+                nk_layout_row_dynamic(ctx, 25, 1);
+                if (nk_combo_begin_color(ctx, nk_rgb_cf(bg), nk_vec2(nk_widget_width(ctx), 400))) {
+                    nk_layout_row_dynamic(ctx, 120, 1);
+                    bg = nk_color_picker(ctx, bg, nk_color_format.NK_RGBA);
+                    nk_layout_row_dynamic(ctx, 25, 1);
+                    bg.r = nk_propertyf(ctx, "#R:", 0, bg.r, 1.0f, 0.01f, 0.005f);
+                    bg.g = nk_propertyf(ctx, "#G:", 0, bg.g, 1.0f, 0.01f, 0.005f);
+                    bg.b = nk_propertyf(ctx, "#B:", 0, bg.b, 1.0f, 0.01f, 0.005f);
+                    bg.a = nk_propertyf(ctx, "#A:", 0, bg.a, 1.0f, 0.01f, 0.005f);
+                    nk_combo_end(ctx);
+                }
+            }
+
             nk_style_pop_float(ctx);
             nk_style_pop_vec2(ctx);
         }
