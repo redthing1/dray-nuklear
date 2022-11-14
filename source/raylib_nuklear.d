3eaae532ec3c21198288f0920f8d1ed668dae587
redthing1 <redthing1@alt.icu>
2022-11-13 19:01:00 -0800

    new nuklear bindings

--- source/raylib_nuklear.d
+++ source/raylib_nuklear.d
@@ -47,16 +47,16 @@ void DrawNuklear (nk_context* ctx); // Render the Nuklear GUI on the screen
 void UnloadNuklear (nk_context* ctx); // Deinitialize the Nuklear context
 nk_color ColorToNuklear (Color color); // Convert a raylib Color to a Nuklear color object
 nk_colorf ColorToNuklearF (Color color); // Convert a raylib Color to a Nuklear floating color
-Color ColorFromNuklear (nk_color color); // Convert a Nuklear color to a raylib Color
-Color ColorFromNuklearF (nk_colorf color); // Convert a Nuklear floating color to a raylib Color
-Rectangle RectangleFromNuklear (nk_rect_ rect); // Convert a Nuklear rectangle to a raylib Rectangle
-nk_rect_ RectangleToNuklear (Rectangle rect); // Convert a raylib Rectangle to a Nuklear Rectangle
-Rectangle RectangleFromNuklearScaled (nk_context* ctx, nk_rect_ rect); // Convert a Nuklear rectangle to a raylib Rectangle with scale compensation
-nk_rect_ RectangleToNuklearScaled (nk_context* ctx, Rectangle rect); // Convert a raylib Rectangle to a Nuklear Rectangle with scale compensation
+Color_ ColorFromNuklear (nk_color color); // Convert a Nuklear color to a raylib Color
+Color_ ColorFromNuklearF (nk_colorf color); // Convert a Nuklear floating color to a raylib Color
+Rectangle_ RectangleFromNuklear (nk_context* ctx, nk_rect_ rect); // Convert a Nuklear rectangle to a raylib Rectangle
+nk_rect_ RectangleToNuklear (nk_context* ctx, Rectangle rect); // Convert a raylib Rectangle to a Nuklear Rectangle
 nk_image_ TextureToNuklear (Texture tex); // Convert a raylib Texture to A Nuklear image
-Texture TextureFromNuklear (nk_image_ img); // Convert a Nuklear image to a raylib Texture
+Texture_ TextureFromNuklear (nk_image_ img); // Convert a Nuklear image to a raylib Texture
 nk_image_ LoadNuklearImage (const(char)* path); // Load a Nuklear image
 void UnloadNuklearImage (nk_image_ img); // Unload a Nuklear image. And free its data
 void CleanupNuklearImage (nk_image_ img); // Frees the data stored by the Nuklear image
+void SetNuklearScaling (nk_context* ctx, float scaling); // Sets the scaling for the given Nuklear context
+float GetNuklearScaling (nk_context* ctx); // Retrieves the scaling of the given Nuklear context
 
 // RAYLIB_NUKLEAR_H
