d4d9a26fb648e304e2afbbeec4dd1c27fc930007
redthing1 <redthing1@alt.icu>
2024-07-14 22:33:39 -0700

    allow mkdir build to fail

--- build-clib.sh
+++ build-clib.sh
@@ -48,7 +48,7 @@ echo "[$HOST] starting build of $LIB_NAME"
 #
 # build the library
 make clean
-mkdir build # create build directory
+mkdir -p build # create build directory
 make $BUILD_ARGS
 #
 # END BUILD
